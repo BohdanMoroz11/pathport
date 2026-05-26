b4f0320ed50fcecae8a0bc5471208e93f9252f4c
bohdanmoroz11 <moroz.bogdan@pm.me>
2026-05-26 12:28:47 +0300

    ?

--- apps/web/next-env.d.ts
+++ apps/web/next-env.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+import "./.next/types/routes.d.ts";
 
-// This file follows Next.js conventions and is kept in the repo for type discovery.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
